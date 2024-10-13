9c83677612b77c637a5d068609946cc9fdcd075f
sonia <stejerec@myuax.com>
2024-10-13 18:29:53 +0200

    Simulación y tabla de Galton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
+        BufferCompartido buffer = new BufferCompartido(10);
 
+        //Creación de productores para diferentes componentes
+        Productor productor1 = new Productor(buffer, "Componente A");
+        Productor productor2 = new Productor(buffer, "Componente B");
+        Productor productor3 = new Productor(buffer, "Componente C");
+
+        //Creación de consumidor (línea de ensamblaje)
+        Consumidor ensamblador = new Consumidor(buffer);
+
+        //Inicio de los hilos
+        productor1.start();
+        productor2.start();
+        productor3.start();
+        ensamblador.start();
     }
 }
